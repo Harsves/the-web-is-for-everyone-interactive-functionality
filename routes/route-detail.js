d1b4739f22d6eae0c886b17ddd6c3fd451851ec9
Suuskehh <suus.harsveld@hva.nl>
2023-03-31 10:04:33 +0200

    Update

---
+++ routes/route-detail.js
@@ -0,0 +1,10 @@
+import * as dotenv from "dotenv";
+
+import express from "express";
+import { fetchJson, postJson } from "../helpers/fetchWrapper.js";
+
+dotenv.config();
+
+
+
+export default routeDetail
--- server.js
+++ server.js
@@ -8,9 +8,6 @@ dotenv.config();
 // Maak een nieuwe express app aan
 const app = express()
 
-const url = `${process.env.API_URL}/stekjes`
-const data = await fetch(url).then(response => response.json())
-
 // Stel ejs in als template engine en geef de 'views' map door
 app.set('view engine', 'ejs')
 app.set('views', './views')
@@ -20,7 +17,10 @@ app.use(express.static('public'))
 
 // Maak een route voor de index
 app.get('/', function (req, res) {
-  res.render('index', data)
+  const url = `${process.env.API_URL}/stekjes`
+  fetchJson(url).then((data) => {
+    res.render('index', data)
+  })
 })
 
 // Maak een route voor het form
@@ -28,9 +28,19 @@ app.get('/form', function (req, res) {
   res.render('form')
 })
 
-// Maak een route voor de detailpage
-app.get('/stekje/:id', function (req, res) {
-  res.render('detail', data)
+/// Maak een route voor de detailpage
+app.get('/stekje', function (req, res) {
+  const url = `${process.env.API_URL}/stekjes?id=${req.query.id}`
+  fetchJson(url).then((data) => {
+    res.render('detail', data)
+  })
+})
+
+app.get('/stekjesbieb', function (req, res) {
+  const url = `${process.env.API_URL}/stekjes`
+  fetchJson(url).then((data) => {
+    res.render('stekjesbieb', data)
+  })
 })
 
 // na submit Stel afhandeling van formulieren in
@@ -46,3 +56,14 @@ app.set('port', 8000)
 // Start express op, haal het ingestelde poortnummer op
 app.listen(app.get('port'), function () {
 })
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+ async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
